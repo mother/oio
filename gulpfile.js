73223ccf819025008131b11d1b9cbcbe743cb008
Jared Reich <jaredreich@gmail.com>
2016-09-20 10:59:13 -0600

    small fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const gulp = require('gulp')
 const less = require('gulp-less')
+const concat = require('gulp-concat')
 
-gulp.task('styles', () => {
+gulp.task('compileCSS', () => {
    gulp.src('./src/foundation/styles.less')
       .pipe(less())
       .pipe(gulp.dest('./dist/'))
 })
 
-gulp.task('default', ['styles'])
+gulp.task('concatLESS', () => {
+   gulp.src(['./src/foundation/*.less', '!./src/foundation/styles.less'])
+      .pipe(concat('styles.less'))
+      .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('default', ['compileCSS'])
