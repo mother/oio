d2f31a88b59aa4d733f0971ff914cecb209bf6f8
Jared Reich <jaredreich@gmail.com>
2016-09-29 15:44:51 -0600

    Added form errors and labels

--- src/components/Form/Input/index.js
+++ src/components/Form/Input/index.js
@@ -1,24 +1,37 @@
 import React from 'react'
 import classNames from 'classnames'
 import styles from './styles.less'
+import formStyles from '../styles.less'
 
-const Input = ({ className, input, placeholder, type, value }) => {
+const Input = ({ className, id, input, label, meta, placeholder, type, value }) => {
    const classes = [styles.input, className]
 
    return (
-      <input
-         type={type}
-         placeholder={placeholder}
-         value={value}
-         className={classNames(classes)}
-         {...input}
-      />
+      <div className={formStyles.container}>
+         {label && <label htmlFor={id}>{label}</label>}
+         <input
+            className={classNames(classes)}
+            id={id}
+            placeholder={placeholder}
+            type={type}
+            value={value}
+            {...input}
+         />
+         {meta && meta.touched && meta.error &&
+            <div className={formStyles.error}>
+               {meta.error}
+            </div>
+         }
+      </div>
    )
 }
 
 Input.propTypes = {
    className: React.PropTypes.string,
+   id: React.PropTypes.string,
    input: React.PropTypes.object,
+   label: React.PropTypes.string,
+   meta: React.PropTypes.object,
    placeholder: React.PropTypes.string,
    type: React.PropTypes.string,
    value: React.PropTypes.string
