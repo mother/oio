78b7c94d096f2256817b885cbfc530b7f28c674e
Steven Yuen <steven@mother.co>
2016-11-16 12:23:46 -0700

    Updated video component to use window resize helpers

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { getWindowSize } from '../../utils/sizeUtils'
 
 export default class Video extends Component {
    static propTypes = {
@@ -29,50 +30,12 @@ export default class Video extends Component {
       window.removeEventListener('resize', this.windowSizeUpdated)
    }
 
-   getAttributeForCurrentSize(attributeValue) {
-      const currentSize = this.state.size
-      const fragments = attributeValue.split(' ')
-
-      for (const fragment of fragments) {
-         const charSet = fragment.match(/\[([abcdef,-]+)\]/i)
-         if (Array.isArray(charSet) && charSet.length === 2) {
-            const charRegexp = new RegExp(`[${charSet[1]}]`, 'i')
-            const match = currentSize.match(charRegexp)
-            if (Array.isArray(match)) {
-               return fragment.replace(charSet[0], '')
-            }
-         } else {
-            return fragment
-         }
-      }
-
-      return null
-   }
-
-   getWindowSize() {
-      const windowWidth = document.documentElement.clientWidth
-
-      if (windowWidth >= 1650) {
-         return 'e'
-      } else if (windowWidth >= 1300) {
-         return 'd'
-      } else if (windowWidth >= 992) {
-         return 'c'
-      } else if (windowWidth >= 768) {
-         return 'b'
-      } else if (windowWidth >= 0) {
-         return 'a'
-      }
-
-      return null
-   }
-
    setVideoContainer() {
 
    }
 
    windowSizeUpdated() {
-      const windowSize = this.getWindowSize()
+      const windowSize = getWindowSize()
       this.setState({ size: windowSize })
    }
 
