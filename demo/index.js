db37837edc2b913a436195045f6be774d06abe55
Ajay Sabhaney <ajay@mothercreative.com>
2016-12-22 16:07:12 -0600

    Update index.js

--- demo/index.js
+++ demo/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import OIO from '../src/components/OIO'
-
 import Demo from './Demo'
 
 ReactDOM.render(<OIO><Demo /></OIO>, document.getElementById('container'))
