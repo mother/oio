a1db1208d6aa61e294d2ab93024afc055fa44e06
Steven Yuen <steven@mother.co>
2016-12-26 13:03:58 -0700

    Updated NavList, Button component and Demo

--- demo/index.js
+++ demo/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, browserHistory } from 'react-router'
 
 import OIO from '../src/components/OIO'
-import Demo from './Demo'
+import routes from './routes'
 
 ReactDOM.render(
    <OIO>
-      <Demo />
+      <Router history={browserHistory} routes={routes} />
    </OIO>
 , document.getElementById('container'))
