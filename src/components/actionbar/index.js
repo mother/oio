cb018933419e0ef3bb8be21f4eae8806d72af7f2
Steven Yuen <steven@mother.co>
2017-01-12 14:16:12 -0700

    Minor UI Updates

--- src/components/actionbar/index.js
+++ src/components/actionbar/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import classNames from 'classnames'
 import styles from './styles.less'
 
-const ActionBar = ({ children }) => (
-   <div className={styles.actionBar}>{children}</div>
-)
+const ActionBar = ({ children, flush }) => {
+   const actionBarStyles = [styles.actionBar]
+
+   if (flush) {
+      actionBarStyles.push(styles.flush)
+   }
+
+   return (
+      <div className={classNames(actionBarStyles)}>{children}</div>
+   )
+}
 
 ActionBar.propTypes = {
-   children: React.PropTypes.node
+   children: React.PropTypes.node,
+   flush: React.PropTypes.bool
 }
 
 export default ActionBar
