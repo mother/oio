9a263967e82f21e3feeeb93cd6f252a20b47cb4d
Steven Yuen <steven@mother.co>
2017-01-18 13:57:41 -0700

    Added OIO Defaults

---
+++ src/components/OIO/defaults.js
@@ -0,0 +1,52 @@
+export default {
+   base: {
+      colors: {
+         primary: '#78909C',
+         none: 'none'
+      },
+      height: {
+         large: '48px',
+         medium: '36px',
+         small: '30px',
+         tiny: '24px'
+      }
+   },
+   font: {
+      family: 'Helvetica Neue, Arial, sans-serif',
+      sizes: {
+         1: {
+            fontSize: ['11px[a] 20px[b-e]'],
+            lineHeight: ['16px']
+         },
+         2: {
+            fontSize: '11px',
+            lineHeight: '16px'
+         },
+         3: {
+            fontSize: '11px',
+            lineHeight: '16px'
+         },
+         4: {
+            fontSize: '11px',
+            lineHeight: '16px'
+         },
+         5: {
+            fontSize: '11px',
+            lineHeight: '16px'
+         },
+         6: {
+            fontSize: '11px',
+            lineHeight: '16px'
+         }
+      }
+   },
+   viewport: {
+      minSizes: {
+         a: '0px',
+         b: '768px',
+         c: '992px',
+         d: '1300px',
+         e: '1650px'
+      }
+   }
+}
