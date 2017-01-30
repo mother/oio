c85e75385b222cf2a20452cc8da2b3860574a520
Jared Reich <jaredreich@gmail.com>
2017-01-30 13:22:08 -0700

    Message styles near completion checkpoint.

--- demo/content/Message.js
+++ demo/content/Message.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import {
+   Button,
    Grid,
    GridCell,
-   // Icon,
+   Message,
    Spacer,
    Text,
    Title,
@@ -17,40 +18,103 @@ export default class DemoContentMessage extends Component {
       contents: React.PropTypes.array
    }
 
+   constructor(props) {
+      super(props)
+
+      this.handleAlertClick = this.handleAlertClick.bind(this)
+
+      this.state = {
+         message: {
+            showing: false,
+            type: 'alert'
+         }
+      }
+   }
+
+   handleAlertClick() {
+      this.setState({
+         message: {
+            ...this.state.message,
+            showing: true
+         }
+      })
+   }
+
    render() {
+      const message = (this.state && this.state.message) || {}
+
       return (
-         <View width="100%" className={style.docs}>
-            <TitleBar title="Message" flush />
-            <Spacer size="9" />
-            <Grid columns="1[a] 2[b] 4[c-e]" gutter="60px">
-               <GridCell>
-                  <Title>Message Configurations</Title>
-                  <Text size="2" color="gray50">
-                     Props for using the <code>Message</code> component
-                  </Text>
-               </GridCell>
-               <GridCell colspan="3">
-                  <table className={style.table}>
-                     <thead>
-                        <tr>
-                           <th width="150px">Prop</th>
-                           <th width="90px">Prop Type</th>
-                           <th width="120px">Default Value</th>
-                           <th>Description/Options</th>
-                        </tr>
-                     </thead>
-                     <tbody>
-                        <tr>
-                           <td><b>propName</b></td>
-                           <td>String</td>
-                           <td><code>0px</code></td>
-                           <td>Description...</td>
-                        </tr>
-                     </tbody>
-                  </table>
-               </GridCell>
-            </Grid>
-         </View>
+         <div>
+            <Message
+               type={message.type}
+               showing={message.showing}
+            />
+            <View width="100%" className={style.docs}>
+               <TitleBar title="Message" flush />
+               <Spacer size="9" />
+               <Grid columns="1[a] 2[b] 4[c-e]" gutter="60px">
+                  <GridCell>
+                     <Title>Message Alert</Title>
+                     <Text size="2" color="gray50">
+                        Displays an alert message with a specified message and an OK button.
+                     </Text>
+                  </GridCell>
+                  <GridCell colspan="3">
+                     <Button onClick={this.handleAlertClick} name="Alert" size="large" />
+                  </GridCell>
+               </Grid>
+               <Grid columns="1[a] 2[b] 4[c-e]" gutter="60px">
+                  <GridCell>
+                     <Title>Message Confirm</Title>
+                     <Text size="2" color="gray50">
+                        Displays a dialog box with a specified message, along with an OK and a Cancel button.
+                     </Text>
+                  </GridCell>
+                  <GridCell colspan="3">
+                     <Button name="Confirm" size="large" />
+                  </GridCell>
+               </Grid>
+               <Grid columns="1[a] 2[b] 4[c-e]" gutter="60px">
+                  <GridCell>
+                     <Title>Message Prompt</Title>
+                     <Text size="2" color="gray50">
+                        Displays a dialog box that prompts the user for input.
+                     </Text>
+                  </GridCell>
+                  <GridCell colspan="3">
+                     <Button name="Prompt" size="large" />
+                  </GridCell>
+               </Grid>
+               <Grid columns="1[a] 2[b] 4[c-e]" gutter="60px">
+                  <GridCell>
+                     <Title>Message Configurations</Title>
+                     <Text size="2" color="gray50">
+                        Props for using the <code>Message</code> component
+                     </Text>
+                  </GridCell>
+                  <GridCell colspan="3">
+                     <table className={style.table}>
+                        <thead>
+                           <tr>
+                              <th width="150px">Prop</th>
+                              <th width="90px">Prop Type</th>
+                              <th width="120px">Default Value</th>
+                              <th>Description/Options</th>
+                           </tr>
+                        </thead>
+                        <tbody>
+                           <tr>
+                              <td><b>propName</b></td>
+                              <td>String</td>
+                              <td><code>0px</code></td>
+                              <td>Description...</td>
+                           </tr>
+                        </tbody>
+                     </table>
+                  </GridCell>
+               </Grid>
+            </View>
+         </div>
       )
    }
 }
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react'
+import classNames from 'classnames'
+
+import Button from '../Button'
+import ButtonGroup from '../ButtonGroup'
+import styles from './styles.less'
 
 export default class Message extends Component {
    static propTypes = {
+      showing: React.PropTypes.bool
    }
 
    static defaultProps = {
+      showing: false
    }
 
    render() {
+      const displayClass = this.props.showing ? 'showMessage' : 'hideMessage'
+      const display = this.props.showing ? 'table' : 'none'
+
       return (
-         <div>hello</div>
+         <div
+            className={classNames(styles.container, styles[displayClass])}
+            style={{ display }}>
+            <div className={styles.containerInner}>
+               <div className={styles.box}>
+                  <div className={styles.header}>
+                     <div className={styles.headerText}>Title</div>
+                     <div className={styles.headerClose}>
+                        <div className={classNames('icon', 'ion-ios-close-empty', styles.headerCloseButton)} />
+                     </div>
+                  </div>
+                  <div className={styles.content}>Message Message Message Message Message Message Message Message Message Message Message Message Message Message Message Message Message Message Message Message Message</div>
+                  <div className={styles.footer}>
+                  <ButtonGroup align="right">
+                     <Button color="#ccc" name="Cancel" />
+                     <Button color="#ccc" name="OK" />
+                  </ButtonGroup>
+                  </div>
+               </div>
+            </div>
+         </div>
       )
    }
 }
