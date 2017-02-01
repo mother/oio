c25eef0212c92233fb1cbacf2499900b8143ccc2
Steven Yuen <steven@mother.co>
2017-01-31 17:51:27 -0700

    Added ImageInput to Form component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -7,6 +7,7 @@ import { findNodesinDOM, replaceNodesInDOM } from '../../utils/dom'
 const formComponentNames = [
    'CheckboxGroup',
    'FileInput',
+   'ImageInput',
    'Input',
    'RadioGroup',
    'Select',
@@ -15,7 +16,8 @@ const formComponentNames = [
 ]
 
 const formFileComponentNames = [
-   'FileInput'
+   'FileInput',
+   'ImageInput'
 ]
 
 const predefinedRules = {
