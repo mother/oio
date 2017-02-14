8079c84f05fbe6ef2771183fa4f2ed5304bf6068
Jared Reich <jaredreich@gmail.com>
2017-02-14 16:49:10 -0700

    Change Text demo handleedit and handle done method names.

--- demo/content/Text.js
+++ demo/content/Text.js
@@ -18,8 +18,8 @@ export default class DemoContentText extends Component {
    constructor(props) {
       super(props)
 
-      this.handleCancel = this.handleCancel.bind(this)
-      this.handleDone = this.handleDone.bind(this)
+      this.handleEditCancel = this.handleEditCancel.bind(this)
+      this.handleEditDone = this.handleEditDone.bind(this)
 
       this.state = {
          value: 'Flexitarian biodiesel kale chips, hoodie lumbersexual food truck keffiyeh umami single-origin coffee franzen. Celiac viral put a bird on it, farm-to-table heirloom everyday carry before they sold out locavore listicle stumptown. Cold-pressed single-origin coffee seitan, next level biodiesel vinyl synth chia pop-up sartorial ugh post-ironic. Hella bitters cardigan affogato selfies thundercats gentrify, man braid schlitz normcore banjo umami messenger bag sartorial. Humblebrag freegan offal, mumblecore tote bag mustache venmo meditation lumbersexual. Put a bird on it intelligentsia lomo gluten-free bitters marfa. Meh literally try-hard ugh everyday carry.',
@@ -28,11 +28,11 @@ export default class DemoContentText extends Component {
       }
    }
 
-   handleCancel(value) {
+   handleEditCancel(value) {
       this.setState({ editing: false })
    }
 
-   handleDone(value) {
+   handleEditDone(value) {
       this.setState({ editing: true, editLoading: true })
       setTimeout(() => {
          this.setState({
@@ -79,8 +79,8 @@ export default class DemoContentText extends Component {
                      editable
                      editorShowEditButton
                      editorLoading={this.state.editLoading}
-                     editorOnCancel={this.handleCancel}
-                     editorOnDone={this.handleDone}
+                     editorOnCancel={this.handleEditCancel}
+                     editorOnDone={this.handleEditDone}
                      editorValue={this.state.value}
                   />
                </GridCell>
