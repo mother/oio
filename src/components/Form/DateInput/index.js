bfc4876ee7fa609f0f25706d9c91105cb6af3267
Jared Reich <jaredreich@gmail.com>
2017-02-22 16:03:11 -0700

    Make DateInput start working with Form (year, month, day).

--- src/components/Form/DateInput/index.js
+++ src/components/Form/DateInput/index.js
@@ -14,27 +14,60 @@ export default class DateInput extends Component {
       label: React.PropTypes.string,
       // name: React.PropTypes.string,
       // onBlur: React.PropTypes.func,
-      // onChange: React.PropTypes.func,
+      onChange: React.PropTypes.func,
       // placeholder: React.PropTypes.string,
       touched: React.PropTypes.bool
       // value: React.PropTypes.string
    }
 
    static defaultProps = {
-      value: ''
+      value: new Date()
    }
 
    static contextTypes = {
       OIOStyles: React.PropTypes.object
    }
 
-   /*
    constructor(props, context) {
       super(props, context)
+
+      this.handleDayChange = this.handleDayChange.bind(this)
+      this.handleYearChange = this.handleYearChange.bind(this)
+
+      const now = new Date()
+      this.state = {
+         day: now.getDate(),
+         month: now.getMonth(),
+         year: now.getFullYear()
+      }
    }
-   */
 
    componentWillReceiveProps(props) {
+   }
+
+   handleChange() {
+      const value = new Date(this.state.year, this.state.month, this.state.day)
+      if (this.props.onChange) {
+         this.props.onChange(null, value)
+      }
+   }
+
+   handleDayChange(event) {
+      let value = event.target.value
+      value = value.replace(/[^\d]/g, '')
+      value = value.replace(/^0+/, '')
+      if (value.length <= 2) {
+         this.setState({ day: value }, () => this.handleChange())
+      }
+   }
+
+   handleYearChange(event) {
+      let value = event.target.value
+      value = value.replace(/[^\d]/g, '')
+      value = value.replace(/^0+/, '')
+      if (value.length <= 4) {
+         this.setState({ year: value }, () => this.handleChange())
+      }
    }
 
    render() {
@@ -42,24 +75,41 @@ export default class DateInput extends Component {
          <div className={formStyles.container}>
             {this.props.label && <label htmlFor={this.props.id}>{this.props.label}</label>}
             <div className={styles.container}>
-               <input className={classNames([styles.input, styles.year])} placeholder="Year" type="tel" />
-               <select className={classNames([styles.select, styles.month])}>
-                  <option value="" disabled selected>Month</option>
-                  <option value="1">January</option>
-                  <option value="2">February</option>
-                  <option value="3">March</option>
-                  <option value="4">April</option>
-                  <option value="5">May</option>
-                  <option value="6">January</option>
-                  <option value="7">June</option>
-                  <option value="8">July</option>
-                  <option value="9">August</option>
-                  <option value="10">September</option>
-                  <option value="11">October</option>
-                  <option value="12">December</option>
+               <input
+                  className={classNames([styles.input, styles.year])}
+                  onChange={this.handleYearChange}
+                  placeholder="Year"
+                  type="tel"
+                  value={this.state.year}
+               />
+               <select
+                  className={classNames([styles.select, styles.month])}
+                  value={this.state.month}>
+                  <option value="0">January</option>
+                  <option value="1">February</option>
+                  <option value="2">March</option>
+                  <option value="3">April</option>
+                  <option value="4">May</option>
+                  <option value="5">January</option>
+                  <option value="6">June</option>
+                  <option value="7">July</option>
+                  <option value="8">August</option>
+                  <option value="9">September</option>
+                  <option value="10">October</option>
+                  <option value="11">December</option>
                </select>
-               <input className={classNames([styles.input, styles.day])} placeholder="D" type="tel" />
+               <input
+                  className={classNames([styles.input, styles.day])}
+                  onChange={this.handleDayChange}
+                  placeholder="Day"
+                  type="tel"
+                  value={this.state.day}
+               />
                <input className={classNames([styles.input, styles.time])} placeholder="Time" type="tel" />
+               <select className={classNames([styles.select, styles.meridiem])}>
+                  <option value="am">am</option>
+                  <option value="pm">pm</option>
+               </select>
             </div>
             {this.props.touched && this.props.error &&
                <div className={formStyles.error}>
