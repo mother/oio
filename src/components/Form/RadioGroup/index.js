6c31f2892faa8826a6cbb5ca84356a6e57801ea3
Jared Reich <jaredreich@gmail.com>
2017-03-06 16:43:13 -0700

    Changes to allow Radio to work in isolation.

--- src/components/Form/RadioGroup/index.js
+++ src/components/Form/RadioGroup/index.js
@@ -47,21 +47,24 @@ export default class RadioGroup extends Component {
    }
 
    componentDidMount() {
-      if (this.props.name) {
+      if (this.context.OIOForm && this.props.name) {
          this.context.OIOForm.setDefaultValue(this.props.name, this.state.value)
          this.context.OIOForm.setRules(this.props.name, this.props.rules)
       }
    }
 
    componentWillReceiveProps(nextProps) {
+      // TODO: If name changes, need to remove form value corresponding to old name
       if (nextProps.value && nextProps.value !== this.state.value) {
          this.setState({ value: nextProps.value })
-         this.context.OIOForm.setValue(this.props.name, nextProps.value)
+
+         if (this.context.OIOForm) {
+            this.context.OIOForm.setValue(this.props.name, nextProps.value)
+            this.setState({
+               error: this.context.OIOForm.getErrors().errors[this.props.name]
+            })
+         }
       }
-
-      this.setState({ error: this.context.OIOForm.getErrors().errors[this.props.name] })
-
-      // TODO: If name changes, need to remove form value corresponding to old name
    }
 
    getValue() {
@@ -70,15 +73,18 @@ export default class RadioGroup extends Component {
 
    handleChange(event) {
       this.setState({ value: event.target.value })
-      this.context.OIOForm.setValue(this.props.name, event.target.value)
 
-      const error = this.context.OIOForm.validateValue(
-         this.props.name,
-         event.target.value,
-         this.props.rules
-      )
+      if (this.context.OIOForm) {
+         this.context.OIOForm.setValue(this.props.name, event.target.value)
 
-      this.setState({ error })
+         const error = this.context.OIOForm.validateValue(
+            this.props.name,
+            event.target.value,
+            this.props.rules
+         )
+
+         this.setState({ error })
+      }
    }
 
    render() {
