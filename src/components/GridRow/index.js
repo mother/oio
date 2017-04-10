566723a5e8b6f1455472e3bffca743e4519533c3
Steven Yuen <steven@mother.co>
2017-04-10 00:49:05 -0600

    Added className prop to GridRow component

--- src/components/GridRow/index.js
+++ src/components/GridRow/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import classNames from 'classnames'
 import style from './style.less'
 
-const GridRow = ({ children }) => (
-   <div className={style.gridRow}>
+const GridRow = ({ children, className }) => (
+   <div className={classNames(style.gridRow, className)}>
       {children}
    </div>
 )
 
 GridRow.propTypes = {
-   children: React.PropTypes.node
+   children: React.PropTypes.node,
+   className: React.PropTypes.string
 }
 
 export default GridRow
